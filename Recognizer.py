c1452c43bb335ae83517abb5f3a638f42ba3ba23
Ameya Phatak <ameyaphatak88@gmail.com>
2020-06-23 11:23:05 +0530

    Recognize expression and draw rectangle

--- Recognizer.py
+++ Recognizer.py
@@ -6,12 +6,9 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 model = FacialExpressionModel("model.json", "model_weights.h5")
 
-def draw_rectangle(face):
-    cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0), 2)
-    return fr
 
 
-def predict(fr):
+def predict_and_rectangle(fr):
     gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
     faces = facec.detectMultiScale(gray_fr, 1.3, 5)
 
@@ -21,5 +18,7 @@ def predict(fr):
 
         pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
 
-        return (faces, pred)
+        cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+        cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
 
+    return fr
